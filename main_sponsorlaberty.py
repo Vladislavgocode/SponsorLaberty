cd8985478e8d824d8ada088346833a3dcfc672a7
Vladislavgocode <bogdanofik@gmail.com>
2022-09-13 11:16:59 +0300

    new_changes

--- main_sponsorlaberty.py
+++ main_sponsorlaberty.py
@@ -95,6 +95,15 @@ async def on_ready():
 				pass
 			connection.commit()
 	print("БОТ АКТИВЕН !")
+	
+@bot.listen()
+async def on_ready():
+    channel = bot.get_channel(1019150040335400962)
+    while True:
+        time.sleep(14400) 
+        embed=discord.Embed(title=f"Внимание Стафф <@999016710990479480>,<@&999016710973689943> ,<@&999016710973689944>,<@&999016710973689942>, !!!", description="Используйте Команду: ``/up или /bump``", color=0x73d216)
+        await channel.send(embed=embed)
+
 @bot.event 
 async def on_member_join(member):
 	sp_date = datetime.now().strftime("%y%m%d%H%M")
